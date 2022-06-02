5cf02b40b1212077f0cc059e16a39d4bb0f23b69
romron@ukr.net <romron@ukr.net>
2022-06-02 16:45:48 +0300

    табсы на js почти готовы

--- Training/portfolio-duplex/portfolio-duplex/js/scripts.js
+++ Training/portfolio-duplex/portfolio-duplex/js/scripts.js
@@ -1,5 +1,31 @@
 
 slider_1();
+tabs_1();
+
+
+
+
+function tabs_1() {
+   const tabber_elements = document.querySelectorAll('.wrap-title-tabs a');
+   const tabs = document.querySelectorAll('.wrap-tabs div');
+   // const title_tab = document.querySelectorAll('wrap-title-tabs div');
+   document.querySelector('.wrap-title-tabs').addEventListener('click', (e) => {
+      for (let i = 0; i < tabs.length; i++) {
+         tabs[i].className = "tab-hide";
+         // title_tab[i].className = "title-tab";
+      }
+      document.querySelector('#tab-' + e.target.id).className = "tab";
+      // document.querySelector('.title-tab-' + e.target.id).className = "title-tab-active";
+   });
+}
+
+
+
+
+
+
+
+
 
 
 function slider_1() {
@@ -10,7 +36,7 @@ function slider_1() {
    let offset = 0;
    const sliderLine = document.querySelector('.wrap-slider-cards');
    let quantity_img = document.querySelectorAll('.wrap-slider-cards .slider-card').length
-   console.log("quantity_img = " + quantity_img)
+   // console.log("quantity_img = " + quantity_img);
 
    document.querySelector('#left').addEventListener('click', function () {
       if (offset > (quantity_img - 5) * 235) {
